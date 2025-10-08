1331c067cb4820cdeadfa3706cf63e935114d80d
Ph4t3 <philippschoenbach1@gmail.com>
2025-10-08 05:25:00 +0200

    erste steps während uebung

---
+++ src/org/hbrs/se1/ws25/exercises/uebung1/control/Factory.java
@@ -0,0 +1,9 @@
+package org.hbrs.se1.ws25.exercises.uebung1.control;
+
+public class Factory {
+    public static void main(String [] args){
+        GermanTranslator hermann = new GermanTranslator();
+        hermann.setDate( "Okt/2025" );
+
+    }
+}
